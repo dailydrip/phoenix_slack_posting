11db566084481c0c8633cead94d41a27bcee4578
Franze Jr <franzejr@gmail.com>
2017-09-29 13:08:01 -0300

    Creating posts if they have a dollar sign

--- lib/slack_posting/robot.ex
+++ lib/slack_posting/robot.ex
@@ -1,3 +1,4 @@
+require IEx
 defmodule SlackPosting.Robot do
   use Hedwig.Robot, otp_app: :slack_posting
 
@@ -14,10 +15,19 @@ defmodule SlackPosting.Robot do
   end
 
   def handle_in(%Hedwig.Message{} = msg, state) do
+    text = msg.text
+    slack_id  = msg.user.id
+    create_post_if_necessary(text, slack_id)
     {:dispatch, msg, state}
   end
 
   def handle_in(_msg, state) do
     {:noreply, state}
+  end
+
+  def create_post_if_necessary(text, slack_id) do
+    if String.contains?(text, "$") do
+      SlackPosting.Journals.create_post(%{text: text, slack_id: slack_id})
+    end
   end
 end
