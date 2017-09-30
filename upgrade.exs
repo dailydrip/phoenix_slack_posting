6010eea0958cffb585753371288c3c7861e08d88
Josh Adams <josh.rubyist@gmail.com>
2017-09-29 23:51:13 -0500

    Make upgrade, deploy files work for assets in 1.3

--- upgrade.exs
+++ upgrade.exs
@@ -2,7 +2,8 @@ defmodule PhoenixSlackPosting.UpgradeCallbacks do
   import Gatling.Bash
 
   def before_mix_digest(env) do
-    bash("npm", ~w[install], cd: env.build_dir)
+		bash("npm", ~w[install], cd: env.build_dir <> "/assets")
+		bash("npm", ~w[run deploy], cd: env.build_dir <> "/assets")
   end
 
   def before_upgrade_service(env) do
