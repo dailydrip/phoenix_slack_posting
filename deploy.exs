b39bc4848c81a3f182c574968b1c3410192b4d85
Josh Adams <josh.rubyist@gmail.com>
2017-09-29 23:14:10 -0500

    Build assets in proper dir

--- deploy.exs
+++ deploy.exs
@@ -3,6 +3,6 @@ defmodule PhoenixSlackPosting.DeployCallbacks do
 
   def before_mix_digest(env) do
     bash("mkdir", ~w[-p priv/static], cd: env.build_dir)
-    bash("npm", ~w[install], cd: env.build_dir)
+		bash("npm", ~w[install], cd: "#{env.build_dir}/assets")
   end
 end
